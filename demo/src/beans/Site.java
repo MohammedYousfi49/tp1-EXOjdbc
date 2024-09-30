e4cd670fd4f0fd5a600d4782cfeec52bacdf43e6
MohammedYousfi49 <mohammed.yousfi554@gmail.com>
2024-09-30 13:17:17 -0400

    Add files via upload

---
+++ demo/src/beans/Site.java
@@ -0,0 +1,36 @@
+
+package beans;
+
+/**
+ *
+ * @author pc
+ */
+public class Site {
+
+    private int id;
+    private String nom;
+
+    public Site() {
+    }
+
+    public Site(String nom) {
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+}
